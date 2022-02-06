f063fc9ee1bca2065f3d5361828bcff4952684a0
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2022-02-06 14:27:18 -0500

    all commits weren't pushed because my dist folder was in gitignore restricted my dist file from pushing. Home page set up, new-patient page set up, files and images added etc

---
+++ dist/assets/script.js
@@ -0,0 +1,49 @@
+// Navbar drop down functionality and mobile toggle button
+
+// grab everything we need
+const btn = document.querySelector("button.mobile-menu-button");
+const menu = document.querySelector(".mobile-menu");
+const dropItem = document.querySelector(".dropdwn-menu-item");
+const dropdwnmenu = document.querySelector(".dropdwn-menu");
+const dropItemMobile = document.querySelector(".dropdwn-menu-item-mobile");
+const dropdwnmenuMobile = document.querySelector(".dropdwn-menu-mobile");
+
+// dropdown mobile
+dropItemMobile.addEventListener("click", () => {
+  dropdwnmenuMobile.classList.toggle("hidden");
+});
+
+// add event listeners
+btn.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
+});
+
+// dropdown
+dropItem.addEventListener("click", () => {
+  dropdwnmenu.classList.toggle("hidden");
+});
+
+// Mapbox
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiYWhpb25pcyIsImEiOiJja3V5cnduYWIzMzNzMnZwZzlna3I4NHFoIn0.097YyIciDtzvJXul-rVwYw";
+var map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11",
+  center: [-76.47251, 39.37124], // starting position [lng, lat]
+  zoom: 14,
+});
+
+const marker = new mapboxgl.Marker()
+  .setLngLat([-76.47251, 39.37124])
+  .addTo(map);
+
+var map2 = new mapboxgl.Map({
+  container: "map2",
+  style: "mapbox://styles/mapbox/streets-v11",
+  center: [-76.47251, 39.37124], // starting position [lng, lat]
+  zoom: 14,
+});
+
+const marker2 = new mapboxgl.Marker()
+  .setLngLat([-76.47251, 39.37124])
+  .addTo(map2);
