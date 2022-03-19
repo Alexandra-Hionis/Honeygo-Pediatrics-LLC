0ace646f3c803f18393fa4c4f897cbc7bf3da0a2
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2022-03-19 00:02:10 -0400

    added h1 to page hero text for SEO purposes

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname + "/dist"));
 // app.use(express.static(__dirname + "/src/styles.css"));
 // sendFile will go here
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "dist/index.html"));
+  res.sendFile(path.join(__dirname, "/dist/index.html"));
 });
 
 app.listen(port);
